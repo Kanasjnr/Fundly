27d179a84ddbccb4ca5b266534479ee65b713b00
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2024-10-30 13:12:20 +0100

    contract address added

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -4,4 +4,4 @@ import abi from "./Fundly.json"
 
 export const contractAbi = abi.abi
 export const desiredNetworkId= 84532
-export const contractAddress = "0x9e7d1274C0C8ef3bE72092903c8c25426e8B0668"
+export const contractAddress = "0xeb28A74a7E33C90F541680c5DAC533228eB8943E"
